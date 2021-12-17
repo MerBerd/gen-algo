3f7760cf745578bc542d3dbef2dca490b3e0e857
MerBerd <mergen00fub@gmail.com>
2021-12-17 16:44:00 +0300

    initial commit

---
+++ util.py
@@ -0,0 +1,19 @@
+import random
+
+class Node:
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    
+    def __str__(self):
+        return str(self.getName())
+
+class Edge:
+    def __init__(self, source, destination)
+
+class Graph:
+    pass
+
+class Sequence:
+    pass
